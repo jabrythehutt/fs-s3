9420a82ab17778fabb503939a26d523174507b28
djabry <djabry@gmail.com>
2016-06-17 10:18:14 +0100

    Changed to using github hosted version of aws-sdk-typescript

--- custom-typings.d.ts
+++ custom-typings.d.ts
@@ -1,6 +1,5 @@
 /**
  * Created by djabry on 21/04/2016.
  */
-/// <reference path="node_modules/@djabry/aws-sdk-typescript/output/typings/aws-sdk.d.ts" />
-/// <reference path="node_modules/@djabry/aws-sdk-typescript/output/typings/aws-s3.d.ts" />
+
 
