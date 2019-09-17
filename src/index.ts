8f956541e3f88e6039f392316931476cefa8cae8
Daniel Jabry <daniel.jabry@rezonence.com>
2019-09-17 12:01:08 +0200

    Correct exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./any-file";
 export * from "./file-service";
+export * from "./ifile-service";
 export * from "./logger";
 export * from "./no.op.logger";
 export * from "./progress-listener";
