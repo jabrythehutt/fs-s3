f7746b0ebc6ee961d06a2f838f4605c5f2b5b283
Daniel Jabry <daniel.jabry@rezonence.com>
2020-06-14 22:54:16 +0100

    #13 #patch Execute publish script in a different way

--- publish.sh
+++ publish.sh
@@ -16,5 +16,5 @@ readonly PKG_NPM_LABELS=`$BAZEL query --output=label 'kind("pkg_npm", //...)'`
 $BAZEL build --config=release $PKG_NPM_LABELS
 # publish one package at a time to make it easier to spot any errors or warnings
 for pkg in $PKG_NPM_LABELS ; do
-  $BAZEL run --config=release -- ${pkg}.${NPM_COMMAND} --access public --tag latest --dry-run
+  $BAZEL run --config=release -- ${pkg}.${NPM_COMMAND} --access public --tag latest
 done
