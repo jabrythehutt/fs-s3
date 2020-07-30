eb956a807ba1abe218097ef739bd802f1a757cd1
Daniel Jabry <daniel.jabry@rezonence.com>
2020-07-30 10:36:39 +0100

    #33 Implement mock node and local file services

--- fs-s3/mock-s3-file-service.ts
+++ fs-s3/mock-s3-file-service.ts
@@ -3,11 +3,17 @@ import {InMemory} from "./in-memory";
 import {Scanner} from "@jabrythehutt/fs-s3-core";
 import {S3WriteOptions} from "./s3-write-options";
 import {S3FileService} from "./s3-file-service";
+import {ScannedS3File} from "@jabrythehutt/fs-s3/scanned-s3-file";
 
 export class MockS3FileService extends InMemory<S3File, S3WriteOptions, S3FileService>(S3FileService) {
 
-    constructor(scanner: Scanner) {
+    constructor(scanner: Scanner, public urlGenerator: (file: ScannedS3File) => Promise<string> =
+        async f => `https://${f.bucket}/${f.key}`) {
         super(scanner);
+    }
+
+    async getReadURLForFile(file: ScannedS3File): Promise<string> {
+        return this.urlGenerator(file);
     }
 
 } 
