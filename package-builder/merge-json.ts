8a59ca491ff39e2b9458cd635e88e13e585adcbc
Daniel Jabry <daniel.jabry@rezonence.com>
2021-01-23 12:03:02 +0000

    Correct json merger types

--- package-builder/merge-json.ts
+++ package-builder/merge-json.ts
@@ -7,7 +7,7 @@ const args = yargs.option("layers", {requiresArg: true, string: true, array: tru
 
 
 const merged = args.layers.map(f => readFileSync(f).toString())
-    .map(s => JSON.parse(s) as object)
+    .map(s => JSON.parse(s) as Record<string, unknown>)
     .reduce((l1, l2) => merge(l1, l2), {});
 
 writeFileSync(args.outputPath, JSON.stringify(merged, null, 4));
