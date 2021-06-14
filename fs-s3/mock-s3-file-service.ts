7487c8f5d2bfa8df73745d9edfcdb22454364cde
Daniel Jabry <djabry@gmail.com>
2021-06-14 17:11:47 +0000

    #56 Correct some absolute imtports

--- fs-s3/mock-s3-file-service.ts
+++ fs-s3/mock-s3-file-service.ts
@@ -3,7 +3,7 @@ import {InMemory} from "./in-memory";
 import {Scanner} from "@jabrythehutt/fs-s3-core";
 import {S3WriteOptions} from "./s3-write-options";
 import {S3FileService} from "./s3-file-service";
-import {ScannedS3File} from "@jabrythehutt/fs-s3/scanned-s3-file";
+import {ScannedS3File} from "./scanned-s3-file";
 
 export class MockS3FileService extends InMemory<S3File, S3WriteOptions, S3FileService>(S3FileService) {
 
